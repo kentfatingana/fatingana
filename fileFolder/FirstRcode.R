04d76c3ed928f81969777167da617d582e85acef
kentfatingana <kent.fatingana@students.isatu.edu.ph>
2026-09-17 09:38:43 +0000

    commit

---
+++ fileFolder/FirstRcode.R
@@ -0,0 +1,15 @@
++joseAge <-10
+juanAge <-20
+
+addAge = joseAge + juanAge
+addAge
+
+mulAge = joseAge * juanAge
+mulAge
+
+subAge = joseAge - juanAge
+subAge
+
+divAge = joseAge / juanAge
+divAge
+
